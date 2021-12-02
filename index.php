fe8e4dce99648305dfe33af686a886c2e77dda8d
trung3am <tqtrung1209@gmail.com>
2021-12-02 23:45:26 +0700

    fix CORS + add seed

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
   <?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 require __DIR__ . "./inc/bootstrap.php";
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
